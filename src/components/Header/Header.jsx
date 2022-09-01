bd3076dff6b3ff565b1745fc81349782db4648da
Matheus Calixto <matheuscalixto8@gmail.com>
2022-09-01 17:23:39 -0300

    feat(header): adicionando contexto de usuario no header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,28 @@
+import { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { ReactComponent as Dogs } from '../../assets/dogs.svg';
+import { UserContext } from '../../contexts/UserContext';
 import styles from './Header.module.css';
 
 const Header = () => {
+  const { data, userLogout } = useContext(UserContext);
+
   return (
     <header className={styles.header}>
       <nav className={`${styles.nav} container`}>
         <Link className={styles.logo} to={'/'} aria-label='Dogs - Home'>
           <Dogs />
         </Link>
-        <Link className={styles.login} to={'/login'}>Login / Criar</Link>
+        {data ? (
+          <Link className={styles.login} to='/conta'>
+            {data.nome}
+            <button onClick={userLogout}>Sair</button>
+          </Link>
+        ) : (
+          <Link className={styles.login} to='/login'>
+            Login / Criar
+          </Link>
+        )}
       </nav>
     </header>
   );
