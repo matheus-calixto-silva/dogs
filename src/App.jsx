434ace590ea7f5744d890804b6b0e4c0c0c5c0ce
Matheus Calixto <matheuscalixto8@gmail.com>
2022-09-01 17:30:28 -0300

    feat: estruturacao das rotas

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
 import './App.css';
 
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import Home from './components/Home/Home';
+import Login from './components/Login/Login';
+import { UserStorage } from './contexts/UserContext';
+
 const App = () => {
   return (
     <div>
-      <h1>Dogs</h1>
+      <BrowserRouter>
+        <UserStorage>
+          <Header />
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/login/*' element={<Login />} />
+          </Routes>
+          <Footer />
+        </UserStorage>
+      </BrowserRouter>
     </div>
   );
 };
