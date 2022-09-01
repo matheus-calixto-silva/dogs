cfaed74235006e33034188163b90ee6168df6cba
Matheus Calixto <matheuscalixto8@gmail.com>
2022-09-01 15:00:48 -0300

    feat: integracao com a api
    todas as funcoes responsaveis por fazer requisicoes a api ficarao em um arquivo separado

---
+++ src/services/dogsService.js
@@ -0,0 +1,32 @@
+import axios from 'axios';
+
+const baseUrl = 'https://dogsapi.origamid.dev/json';
+
+const getToken = async (userData) => {
+  const request = await axios.post(`${baseUrl}/jwt-auth/v1/token`, userData);
+  return request.data;
+};
+
+const tokenValidate = async (token) => {
+  const request = await axios.post(
+    `${baseUrl}/jwt-auth/v1/token/validate`,
+    token,
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  );
+  return request.data;
+};
+
+const getUser = async (token) => {
+  const request = await axios.get(`${baseUrl}/api/user`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  return request.data;
+};
+
+export { getToken, tokenValidate, getUser };
