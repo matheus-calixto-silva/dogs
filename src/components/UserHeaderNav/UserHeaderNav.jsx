6e84d3fc2ddfdeb1f57afd85b8d7a8beac17dc64
Matheus Calixto <matheuscalixto8@gmail.com>
2022-09-12 12:20:05 -0300

    feat(userheadernav): header de usuario, disponivel apenas para usuarios logados

---
+++ src/components/UserHeaderNav/UserHeaderNav.jsx
@@ -0,0 +1,60 @@
+import { useContext, useState } from 'react';
+import { NavLink, useLocation } from 'react-router-dom';
+import { UserContext } from '../../contexts/UserContext';
+import { ReactComponent as MinhasFotos } from '../../assets/feed.svg';
+import { ReactComponent as Estatisticas } from '../../assets/estatisticas.svg';
+import { ReactComponent as AdicionarFoto } from '../../assets/adicionar.svg';
+import { ReactComponent as Sair } from '../../assets/sair.svg';
+import useMedia from '../../hooks/useMedia';
+
+import styles from './UserHeaderNav.module.css';
+import { useEffect } from 'react';
+
+const UserHeaderNav = () => {
+  const [mobileMenu, setMobileMenu] = useState(false);
+  const mobile = useMedia('(max-width: 40rem)');
+  const { userLogout } = useContext(UserContext);
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    setMobileMenu(false);
+  }, [pathname]);
+
+  return (
+    <>
+      {mobile && (
+        <button
+          aria-label='Menu'
+          className={`${styles.mobileMenu} ${
+            mobileMenu && styles.mobileButtonActive
+          }`}
+          onClick={() => setMobileMenu(!mobileMenu)}
+        ></button>
+      )}
+      <nav
+        className={`${mobile ? styles.navMobile : styles.nav} ${
+          mobileMenu && styles.navMobileActive
+        }`}
+      >
+        <NavLink to={'/conta'} end>
+          <MinhasFotos />
+          {mobile && 'Minhas Fotos'}
+        </NavLink>
+        <NavLink to={'/conta/estatisticas'}>
+          <Estatisticas />
+          {mobile && 'Estatísticas'}
+        </NavLink>
+        <NavLink to={'/conta/postar'}>
+          <AdicionarFoto />
+          {mobile && 'Adicionar Foto'}
+        </NavLink>
+        <button onClick={userLogout}>
+          <Sair />
+          {mobile && 'Sair'}
+        </button>
+      </nav>
+    </>
+  );
+};
+
+export default UserHeaderNav;
