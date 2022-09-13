d60dc74876b9cf528c62b4c927d45c31172d2adb
Matheus Calixto <matheuscalixto8@gmail.com>
2022-09-13 18:50:34 -0300

    feat(feedmodal): componente feedmodal
    ao clicar numa foto do feed o modal aparece com a foto clicada, além de detalhes e comentarios

---
+++ src/components/FeedModal/FeedModal.jsx
@@ -0,0 +1,36 @@
+import { useEffect } from 'react';
+
+import Error from '../Error/Error';
+import Loading from '../Loading/Loading';
+import PhotoContent from '../PhotoContent/PhotoContent';
+
+import useFetch from '../../hooks/useFetch';
+
+import { getPhoto } from '../../services/dogsService';
+
+import styles from './FeedModal.module.css';
+
+const FeedModal = ({ photo, setModalPhoto }) => {
+  const { data, error, loading, request } = useFetch();
+  console.log(data);
+
+  useEffect(() => {
+    request(getPhoto, photo.id);
+  }, [photo, request]);
+
+  const handleOutsideClick = ({ target, currentTarget }) => {
+    if (target === currentTarget) {
+      setModalPhoto(null);
+    }
+  };
+
+  return (
+    <div className={styles.modal} onClick={handleOutsideClick}>
+      {error && <Error error={error} />}
+      {loading && <Loading />}
+      {data && <PhotoContent data={data} />}
+    </div>
+  );
+};
+
+export default FeedModal;
