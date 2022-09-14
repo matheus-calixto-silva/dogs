316e3f6c0956c256dbb6a7bcc260a7218f8bcc20
Matheus Calixto <matheuscalixto8@gmail.com>
2022-09-14 15:26:17 -0300

    feat(photocomments): componente responsavel por listar os comentarios

---
+++ src/components/PhotoComments/PhotoComments.jsx
@@ -0,0 +1,33 @@
+import { useState, useContext, useRef, useEffect } from 'react';
+
+import { UserContext } from '../../contexts/UserContext';
+
+import PhotoCommentsForm from '../PhotoCommentsForm/PhotoCommentsForm';
+
+import styles from './PhotoComments.module.css';
+
+const PhotoComments = (props) => {
+  const [comments, setComments] = useState(() => props.comments);
+  const commentsSection = useRef(null);
+  const { login } = useContext(UserContext);
+
+  useEffect(() => {
+    commentsSection.current.scrollTop = commentsSection.current.scrollHeight;
+  }, [comments]);
+
+  return (
+    <>
+      <ul ref={commentsSection} className={styles.comments}>
+        {comments.map((comment) => (
+          <li key={comment.comment_ID}>
+            <b>{comment.comment_author}</b>
+            <span>{comment.comment_content}</span>
+          </li>
+        ))}
+      </ul>
+      {login && <PhotoCommentsForm id={props.id} setComments={setComments} />}
+    </>
+  );
+};
+
+export default PhotoComments;
