c6d77e3df4c69b3680be239c898395fcd56bb5f7
Matheus Calixto <matheuscalixto8@gmail.com>
2022-09-14 12:19:11 -0300

    feat(feedphotositem): adicao de evento de clique as fotos do feed

--- src/components/FeedPhotosItem/FeedPhotosItem.jsx
+++ src/components/FeedPhotosItem/FeedPhotosItem.jsx
@@ -1,8 +1,10 @@
 import styles from './FeedPhotosItem.module.css';
 
-const FeedPhotosItem = ({ photo }) => {
+const FeedPhotosItem = ({ photo, setModalPhoto }) => {
+  const handleClick = () => setModalPhoto(photo);
+
   return (
-    <li className={styles.photo}>
+    <li className={styles.photo} onClick={handleClick}>
       <img src={photo.src} alt={photo.title} />
       <span className={styles.views}>{photo.acessos}</span>
     </li>
