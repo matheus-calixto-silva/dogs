07ddd2bb050228c91317e15eb0a528ffbbdb1ca8
Matheus Calixto <matheuscalixto8@gmail.com>
2022-09-15 15:29:53 -0300

    feat(feedphotositem): uso do componente image, para carregamento personalizado de imagens

--- src/components/FeedPhotosItem/FeedPhotosItem.jsx
+++ src/components/FeedPhotosItem/FeedPhotosItem.jsx
@@ -1,3 +1,4 @@
+import Image from '../Image/Image';
 import styles from './FeedPhotosItem.module.css';
 
 const FeedPhotosItem = ({ photo, setModalPhoto }) => {
@@ -5,7 +6,7 @@ const FeedPhotosItem = ({ photo, setModalPhoto }) => {
 
   return (
     <li className={styles.photo} onClick={handleClick}>
-      <img src={photo.src} alt={photo.title} />
+      <Image src={photo.src} alt={photo.title} />
       <span className={styles.views}>{photo.acessos}</span>
     </li>
   );
