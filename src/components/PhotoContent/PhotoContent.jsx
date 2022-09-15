8f24bdf8a0a45a12c9695c773f34f2cab97be22b
Matheus Calixto <matheuscalixto8@gmail.com>
2022-09-15 15:37:53 -0300

    feat(photocontent): atualizacao no layout do componente
    adicao dos componentes image e photodelete

--- src/components/PhotoContent/PhotoContent.jsx
+++ src/components/PhotoContent/PhotoContent.jsx
@@ -1,19 +1,31 @@
+import { useContext } from 'react';
 import { Link } from 'react-router-dom';
+
+import { UserContext } from '../../contexts/UserContext';
+
+import Image from '../Image/Image';
 import PhotoComments from '../PhotoComments/PhotoComments';
+import PhotoDelete from '../PhotoDelete/PhotoDelete';
+
 import styles from './PhotoContent.module.css';
 
 const PhotoContent = ({ data }) => {
+  const user = useContext(UserContext);
   const { photo, comments } = data;
 
   return (
     <div className={styles.photo}>
       <div className={styles.img}>
-        <img src={photo.src} alt={photo.title} />
+        <Image src={photo.src} alt={photo.title} />
       </div>
       <div className={styles.details}>
         <div>
           <p className={styles.author}>
-            <Link to={`/perfil/${photo.author}`}>@{photo.author}</Link>
+            {user.data && user.data.username === photo.author ? (
+              <PhotoDelete id={photo.id} />
+            ) : (
+              <Link to={`/perfil/${photo.author}`}>@{photo.author}</Link>
+            )}
             <span className={styles.visualizacoes}>{photo.acessos}</span>
           </p>
           <h1 className='title'>
