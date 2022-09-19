d6d8521c5875b33592b494fe58fe3982b70f7f91
Matheus Calixto <matheuscalixto8@gmail.com>
2022-09-19 17:44:39 -0300

    feat: componente photo

---
+++ src/components/Photo/Photo.jsx
@@ -0,0 +1,34 @@
+import { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+
+import Error from '../Error/Error';
+import Loading from '../Loading/Loading';
+import PhotoContent from '../PhotoContent/PhotoContent';
+
+import useFetch from '../../hooks/useFetch';
+
+import { getPhoto } from '../../services/dogsService';
+
+const Photo = () => {
+  const { id } = useParams();
+  const { data, loading, error, request } = useFetch();
+
+  useEffect(() => {
+    const photo = async () => {
+      await request(getPhoto, id);
+    };
+    photo();
+  }, [id, request]);
+
+  if (error) return <Error error={error} />;
+  if (loading) return <Loading />;
+  if (data)
+    return (
+      <section className='container mainContainer'>
+        <PhotoContent data={data} single={true} />
+      </section>
+    );
+  else return null;
+};
+
+export default Photo;
