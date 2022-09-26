7147b5d203fd18835934e7e9a2663e6caa9ac5a3
Matheus Calixto <matheuscalixto8@gmail.com>
2022-09-26 17:57:08 -0300

    feat(loginpasswordlost): componente para solicitar alteracao de senha

---
+++ src/components/LoginPasswordLost/LoginPasswordLost.jsx
@@ -0,0 +1,48 @@
+import Input from '../Input/Input';
+import Button from '../Button/Button';
+import Error from '../Error/Error';
+
+import useForm from '../../hooks/useForm.js';
+import useFetch from '../../hooks/useFetch.js';
+
+import { passwordLost } from '../../services/dogsService';
+
+const LoginPasswordLost = () => {
+  const login = useForm('');
+  const { data, loading, error, request } = useFetch();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    await request(passwordLost, {
+      login: login.value,
+      url: window.location.href.replace('perdeu', 'resetar'),
+    });
+  };
+
+  return (
+    <section>
+      <h1 className='title'>Perdeu a senha?</h1>
+      {data ? (
+        <p style={{ color: '#4c1' }}>{data}</p>
+      ) : (
+        <form onSubmit={handleSubmit}>
+          <Input
+            label={'Email / Usuário'}
+            type={'text'}
+            name={'login'}
+            {...login}
+          />
+          {loading ? (
+            <Button disabled>Enviando...</Button>
+          ) : (
+            <Button>Enviar email</Button>
+          )}
+        </form>
+      )}
+      {<Error error={error} />}
+    </section>
+  );
+};
+
+export default LoginPasswordLost;
