b5b03142b0ed0090ec9a4b2cb35ca7bcb233b2be
Matheus Calixto <matheuscalixto8@gmail.com>
2022-09-27 14:53:04 -0300

    feat(userstats): componente responsavel por exibir os graficos

--- src/components/UserStats/UserStats.jsx
+++ src/components/UserStats/UserStats.jsx
@@ -1,12 +1,35 @@
+import { useEffect } from 'react';
+
 import Head from '../Head/Head';
+import Loading from '../Loading/Loading';
+import Error from '../Error/Error';
+import UserStatsGraphs from '../UserStatsGraphs/UserStatsGraphs';
+
+import useFetch from '../../hooks/useFetch';
+
+import { getStats } from '../../services/dogsService';
 
 const UserStats = () => {
-  return (
-    <div>
-      <Head title={'Estatisticas'} />
-      Estatisticas
-    </div>
-  );
+  const { data, error, loading, request } = useFetch();
+
+  useEffect(() => {
+    const getData = async () => {
+      const token = window.localStorage.getItem('token');
+      await request(getStats, token);
+    };
+    getData();
+  }, [request]);
+
+  if (loading) return <Loading />;
+  if (error) return <Error error={error} />;
+  if (data)
+    return (
+      <div>
+        <Head title={'Estatisticas'} />
+        <UserStatsGraphs data={data} />
+      </div>
+    );
+  else return null;
 };
 
 export default UserStats;
